e99526b4a646cbac3ec10896663138c3ac45577f
馬場英次郎 <33510560+horsejiro@users.noreply.github.com>
2021-11-04 20:16:56 +0900

    add search error commit

--- src/component/Search.tsx
+++ src/component/Search.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-no-bind */
+import moment from 'moment';
 import React, { VFC, useState } from 'react';
 import { DateRangePicker } from 'react-date-range';
 import 'react-date-range/dist/styles.css';
@@ -5,8 +7,8 @@ import 'react-date-range/dist/theme/default.css';
 import './Search.css';
 
 const Search: VFC = () => {
-  const [startDate, setStartDate] = useState(new Date());
-  const [endDate, setEndDate] = useState(new Date());
+  const [startDate, setStartDate] = useState<moment.Moment | null>(null);
+  const [endDate, setEndDate] = useState<moment.Moment | null>(null);
 
   const selectionRange = {
     startDate,
@@ -14,7 +16,21 @@ const Search: VFC = () => {
     key: 'selection',
   };
 
-  return <div className="search" />;
+  function handleSelect(ranges: {
+    selection: {
+      startDate: React.SetStateAction<moment.Moment | null>;
+      endDate: React.SetStateAction<moment.Moment | null>;
+    };
+  }) {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  }
+
+  return (
+    <div className="search">
+      <DateRangePicker ranges={[selectionRange]} onChange={handleSelect} />
+    </div>
+  );
 };
 
 export default Search;

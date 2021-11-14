6b64b01305faf2839db42da2570a927b2efe2433
馬場英次郎 <33510560+horsejiro@users.noreply.github.com>
2021-11-14 15:04:30 +0900

    add card 1

--- src/component/Card.tsx
+++ src/component/Card.tsx
@@ -1,28 +1,31 @@
-import React, { Props, VFC } from 'react';
-import './.Card.css';
+import React, { VFC } from 'react';
+import './Card.css';
 
 export type CardDetails = {
+  id?: number;
   src: string;
   title: string;
-  descripiton: string;
+  description: string;
 };
 
 type Props = {
   cardDetails: CardDetails[];
 };
+
 const Card: VFC<Props> = (props) => {
   const { cardDetails } = props;
 
   return (
     <>
-      <div className="card">
-        {}
-        <img src={src} alt="" />
-        <div className="card__info">
-          <h2>{title}</h2>
-          <h4>{descriotion}</h4>
+      {cardDetails.map((cardDetail) => (
+        <div className="card">
+          <img src={cardDetail.src} alt="" />
+          <div className="card__info">
+            <h2>{cardDetail.title}</h2>
+            <h4>{cardDetail.description}</h4>
+          </div>
         </div>
-      </div>
+      ))}
     </>
   );
 };
--- src/component/Home.tsx
+++ src/component/Home.tsx
@@ -1,36 +1,40 @@
-import React, { FC } from 'react';
+import React, { VFC } from 'react';
 import './Home.css';
 import Banner from './Banner';
-import Card from './Card';
+import Card, { CardDetails } from './Card';
 
-const Home: FC = () => (
-  <div className="home">
-    <h1>Home Component</h1>
+const Home: VFC = () => {
+  const cardDetail: CardDetails[] = [
+    {
+      id: 1,
+      src: 'https://a0.muscache.com/im/pictures/eb9c7c6a-ee33-414a-b1ba-14e8860d59b3.jpg?im_w=720',
+      title: 'Online Experiences',
+      description:
+        'Unique activities we can do together, led by a world of hosts.',
+    },
+    {
+      id: 2,
+      src: 'https://a0.muscache.com/im/pictures/15159c9c-9cf1-400e-b809-4e13f286fa38.jpg?im_w=720',
+      title: 'Unique stays',
+      description: 'Spaces that are more than just a place to sleep.',
+    },
+    {
+      id: 3,
+      src: 'https://a0.muscache.com/im/pictures/fdb46962-10c1-45fc-a228-d0b055411448.jpg?im_w=720',
+      title: 'Entire homes',
+      description:
+        'Comfortable private places, with room for friends or family.',
+    },
+  ];
 
-    <Banner />
-    <div className="home__section">
-      <Card
-        src="https://a0.muscache.com/im/pictures/eb9c7c6a-ee33-414a-b1ba-14e8860d59b3.jpg?im_w=720"
-        title="Online Experiences"
-        description="Unique activities we can do together, led by a world of hosts."
-      />
-      <Card
-        src="https://a0.muscache.com/im/pictures/15159c9c-9cf1-400e-b809-4e13f286fa38.jpg?im_w=720"
-        title="Unique stays"
-        description="Spaces that are more than just a place to sleep."
-      />
-      <Card
-        src="https://a0.muscache.com/im/pictures/fdb46962-10c1-45fc-a228-d0b055411448.jpg?im_w=720"
-        title="Entire homes"
-        description="Comfortable private places, with room for friends or family."
-      />
+  return (
+    <div className="home">
+      <Banner />
+      <div className="home__section">
+        <Card cardDetails={cardDetail} />
+      </div>
     </div>
-    <div className="home__section">
-      <Card />
-      <Card />
-      <Card />
-    </div>
-  </div>
-);
+  );
+};
 
 export default Home;

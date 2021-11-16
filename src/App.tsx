f99e57c9a303ee2cfcbe7bd30c6a96661586f1b5
馬場英次郎 <33510560+horsejiro@users.noreply.github.com>
2021-11-16 21:11:47 +0900

    add footer and final UP Card component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { FC } from 'react';
 import './App.css';
 import Home from './component/Home';
 import Header from './component/Header';
+import Footer from './component/Footer';
 
 const App: FC = () => (
   <div className="App">
     <Header />
     <Home />
+    <Footer />
   </div>
 );
 
---
+++ src/component/Footer.tsx
@@ -0,0 +1,11 @@
+import React, { VFC } from 'react';
+import './Footer.css';
+
+const Footer: VFC = () => (
+  <div className="footer">
+    <p>© 2021 Airbnb cloneです。これはデモです。</p>
+    <p>個人情報管理・サイトマップ・弊社に関して</p>
+  </div>
+);
+
+export default Footer;

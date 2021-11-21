9620fe5e6d8b439127190905c328651a9ce93e9a
馬場英次郎 <33510560+horsejiro@users.noreply.github.com>
2021-11-21 13:07:39 +0900

    update search date

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,16 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './component/Home';
 import Header from './component/Header';
 import Footer from './component/Footer';
+import SearchPage from './component/SearchPage';
 
 const App: FC = () => (
   <div className="App">
     <Router>
       <Header />
       <Switch>
+        <Route path="/search">
+          <SearchPage />
+        </Route>
         <Route path="">
           <Home />
         </Route>

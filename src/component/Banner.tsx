8db11495c53a5cfc2c7fbd8d22bdc0615e0357eb
Taimeshi <programjeh@gmail.com>
2022-05-06 23:53:50 +0900

    no

--- src/component/Banner.tsx
+++ src/component/Banner.tsx
@@ -11,7 +11,7 @@ const Banner: VFC = () => {
   const history = useHistory();
   const [showSearch, setShowSearch] = useState<boolean>(false);
   const toSearch = () => {
-    history.push('./search');
+    history.push(');
   };
 
   return (

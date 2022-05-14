5d7e94ada8cafca2f5b5a998fdfabe9570a73f7a
Taimeshi <programjeh@gmail.com>
2022-05-15 00:32:44 +0900

    history

--- src/component/Banner.tsx
+++ src/component/Banner.tsx
@@ -8,7 +8,6 @@ import './Banner.css';
 import Search from './Search';
 
 const Banner: VFC = () => {
-  const history = useHistory();
   const [showSearch, setShowSearch] = useState<boolean>(false);
   const toSearch = () => {
     history.push('./search');

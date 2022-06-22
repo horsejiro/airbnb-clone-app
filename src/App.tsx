221d1c2b8af1b96ddbb1cb278f92728a567dcba9
Taimeshi <programjeh@gmail.com>
2022-06-22 19:24:01 +0900

    no

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 import './App.css';
-import { BrowserRouter as Router, , Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './component/Home';
 import Header from './component/Header';
 import Footer from './component/Footer';
